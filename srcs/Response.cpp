5199e97ddea36016c87440c245927ba0f20fba17
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2022-02-10 17:13:46 +0900

    response serializa method

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -10,5 +10,15 @@ Response::~Response()
 
 std::string Response::serialize()
 {
+	std::string result;
 
+	result.append("HTTP/1.1 " + status_code + "\r\n");
+	for (std::pair<std::string, std::string> header : headers)
+	{
+		result.append(header.first + ":" + header.second + "\r\n");	
+	}
+	result.append("\r\n");
+	result.append(body);
+
+	return result;
 }
