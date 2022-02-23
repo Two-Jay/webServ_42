f09242f144b0b810a77dfc66dd7b683251c42d7d
안영선 <hi0seon@an-yeongseon-ui-MacBookPro.local>
2022-02-23 15:47:06 +0900

    오타 수정

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -14,7 +14,7 @@ std::string Response::serialize()
 
 	result.append("HTTP/1.1 " + status_code + "\r\n");
 	for (std::map<std::string, std::string>::iterator i = headers.begin(); i != headers.end(); i++)
-	{\
+	{
 		result.append((*i).first + ":" + (*i).second + "\r\n");	
 	}
 	result.append("\r\n");
