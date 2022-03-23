e0ec1b4ede33795955bb57a77eb638a10dc66de3
Ahn YoungSeon <dudtjs0920@naver.com>
2022-03-23 18:34:00 +0900

    Cgi env add

--- srcs/CgiHandler.cpp
+++ srcs/CgiHandler.cpp
@@ -8,8 +8,14 @@ CgiHandler::CgiHandler(Request &request) {
 	this->env["SERVER_PORT"] = request.headers[""];
 	this->env["REQUEST_METHOD"] = request.headers["Request Method"];
 	this->env["PATH_INFO"] = request.getPath();
-	this->env["PATH_TRANSLATED"]  = request.getPath();
+	this->env["PATH_TRANSLATED"] = request.getPath();
 	this->env["SCRIPT_NAME"] = request.getPath();
 	this->env["QUERY_STRING"] = request.getQuery();
 	this->env["REMOTE_HOST"] = request.headers["Host"];
+	this->env["REMOTE_ADDR"] = ;
+	this->env["AUTH_TYPE"] = ;
+	this->env["REMOTE_USER"];
+	this->env["REMOTE_IDENT"];
+	this->env["CONTENT_TYPE"];
+	this->env["CONTENT_LENGTH"];
 }
