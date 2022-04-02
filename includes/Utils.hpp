503f3dd8a661a8fc2408a7254112df94eb792e3e
Ahn YoungSeon <dudtjs0920@naver.com>
2022-04-02 20:41:01 +0900

    add map outstream method

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <sstream>
 #include <dirent.h>
+#include <map>
 
 int replace(std::string &original, std::string word1, std::string word2);
 std::string dir_listing();
@@ -17,6 +18,16 @@ std::ostream &operator<<(std::ostream &out, const std::vector<T> &op)
 	for (int i = 0; i < op.size(); i++)
 	{
 		out << op[i] << "(" << i << ") ";
+	}
+	return out;
+}
+
+template<typename T1, typename T2>
+std::ostream &operator<<(std::ostream &out, const std::map<T1, T2> &op)
+{
+	for (typename std::map<T1, T2>::const_iterator i = op.begin(); i != op.end(); i++)
+	{
+		out << "[ " << (*i).first << " ] = " << (*i).second << "\n";
 	}
 	return out;
 }
