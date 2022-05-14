027a4e5fe67b664aee91c3f4e45c13cd9b39af4a
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-05-14 20:04:59 +0900

    redirection : 헤더에 send_redirection() 추가

--- includes/ServerManager.hpp
+++ includes/ServerManager.hpp
@@ -37,6 +37,7 @@ public:
 	void print_servers_info();
 
 private:
+	void send_redirection(int code, Client &client);
 	void send_error_page(int code, Client &client);
 	bool handle_CGI(Request *request, Location *loc);
 	bool is_response_timeout(Client& client);
